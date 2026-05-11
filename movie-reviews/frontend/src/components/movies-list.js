a53fde378b194b9957babc071293a23c0adc7c62
Celirous <aj.bellringer@gmail.com>
2026-05-11 09:38:16 +0200

    Project complete

--- movie-reviews/frontend/src/components/movies-list.js
+++ movie-reviews/frontend/src/components/movies-list.js
@@ -15,16 +15,32 @@ const MoviesList = (props) => {
   const [searchRating, setSearchRating] = useState("");
   const [ratings, setRatings] = useState(["All Ratings"]);
 
+  const [currentPage, setCurrentPage] = useState(0);
+  const [entriesPerPage, setEntriesPerPage] = useState(0);
+  const [currentSearchMode, setCurrentSearchMode] = useState("");
+
   useEffect(() => {
     retrieveMovies();
     retrieveRatings();
   }, []);
 
+  useEffect(() => {
+    retrieveNextPage();
+  }, [currentPage]);
+
+  const retrieveNextPage = () => {
+    if (currentSearchMode === "findByTitle") findByTitle();
+    else if (currentSearchMode === "findByRating") findByRating();
+    else retrieveMovies();
+  };
+
   const retrieveMovies = () => {
-    MovieDataService.getAll()
+    setCurrentSearchMode("");
+    MovieDataService.getAll(currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
+        setEntriesPerPage(response.data.entries_per_page);
       })
       .catch((e) => {
         console.log(e);
@@ -43,7 +59,7 @@ const MoviesList = (props) => {
   };
 
   const find = (query, by) => {
-    MovieDataService.find(query, by)
+    MovieDataService.find(query, by, currentPage)
       .then((response) => {
         console.log(response.data);
         setMovies(response.data.movies);
@@ -54,10 +70,12 @@ const MoviesList = (props) => {
   };
 
   const findByTitle = () => {
+    setCurrentSearchMode("findByTitle");
     find(searchTitle, "title");
   };
 
   const findByRating = () => {
+    setCurrentSearchMode("findByRating");
     if (searchRating === "All Ratings") {
       retrieveMovies();
     } else {
@@ -94,9 +112,9 @@ const MoviesList = (props) => {
             <Col>
               <Form.Group>
                 <Form.Control as="select" onChange={onChangeSearchRating}>
-                  {ratings.map((rating) => {
-                    return <option value={rating}>{rating}</option>;
-                  })}
+                  {ratings.map((rating) => (
+                    <option key={rating} value={rating}>{rating}</option>
+                  ))}
                 </Form.Control>
               </Form.Group>
               <Button variant="primary" type="button" onClick={findByRating}>
@@ -105,24 +123,31 @@ const MoviesList = (props) => {
             </Col>
           </Row>
         </Form>
-
         <Row>
-          {movies.map((movie) => {
-            return (
-              <Col>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img src={movie.poster + "/100px180"} />
-                  <Card.Body>
-                    <Card.Title>{movie.title}</Card.Title>
-                    <Card.Text>Rating: {movie.rated}</Card.Text>
-                    <Card.Text>{movie.plot}</Card.Text>
-                    <Link to={"/movies/" + movie._id}>View Reviews</Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
+          {movies.map((movie) => (
+            <Col key={movie._id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img src={movie.poster} />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Card.Text>Rating: {movie.rated}</Card.Text>
+                  <Card.Text>{movie.plot}</Card.Text>
+                  <Link to={"/movies/" + movie._id}>View Reviews</Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
+        <br />
+        Showing page: {currentPage}.
+        <Button
+          variant="link"
+          onClick={() => {
+            setCurrentPage(currentPage + 1);
+          }}
+        >
+          Get next {entriesPerPage} results
+        </Button>
       </Container>
     </div>
   );
