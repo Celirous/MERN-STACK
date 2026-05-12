7d21b70dc1aa3f46e792f1be11c862ff4684da21
Celirous <aj.bellringer@gmail.com>
2026-05-12 08:35:17 +0200

    BookStore app

---
+++ BookStore/backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://ajbellringer_db_user:adminKey@book-store.yt33awr.mongodb.net/Book-Store?retryWrites=true&w=majority";
+  
+// Please create a free database for yourself'
+// This will be deleted after
